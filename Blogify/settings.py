0c5086b06286f9a7c0bc7d4a5328a173c1ae393b
waCira <wachura11t@gmail.com>
2021-05-11 04:04:36 +0300

    added django-heroku

--- Blogify/settings.py
+++ Blogify/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,5 +148,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 WS_DEFAULT_ACL = None
 
-
+django_heroku.settings(locals())
 
